ff1e2b26db3bd0666dd787c5863c03004216779e
hahajh0202 <hahajh0202@naver.com>
2025-06-22 15:43:50 +0900

    Start Condition pracitce.

---
+++ condition.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+
+    // int age = 25;
+    // if (age > 20){
+    //     printf("일반인 입니다.\n");
+    // }
+    // else{
+    //     printf("학생 입니다.\n");
+    // }
+
+    int age;
+    printf("나이를 입력하시오: ");
+    scanf("%d", &age);
+  
+    if(age >= 8 && age <=13){
+        printf("초등학생입니다.\n");
+    }
+    else if(age >= 14 && age <=16){
+        printf("중학생입니다.\n");
+    }
+    else if(age >= 17 && age <=19){
+        printf("고등학생입니다.\n");
+    }
+    else{
+        printf("성인입니다.\n");
+    }
+
+    return 0;
+}
